a3a64eb7b739e3b73d31f21ab19f3bd7c8fd423f
Liam Miguel A. Supremo <lasupremo@student.apc.edu.ph>
2024-01-12 23:15:22 +0800

    Update script.js

--- week5/script.js
+++ week5/script.js
@@ -1,11 +1,11 @@
+function updateFooterYear() {
+    const currentYear = new Date().getFullYear();
+    document.querySelector('footer p').textContent += ` ${currentYear}`;
+}
+
 function toggleMenu () {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
     icon.classList.toggle("open")
-}
-
-function updateFooterYear () {
-    const currentYear = new Date().getFullYear();
-    document.querySelector('footer p').textContent += ` ${currentYear}`;
 }

4358f628b9a75d7374e725f7867653b616c3e943
Liam Miguel A. Supremo <lasupremo@student.apc.edu.ph>
2024-02-08 15:33:32 +0800

    updated selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender, reg_date FROM lasupremo_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM lasupremo_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Website: " . $row["website"]. " - Comment: " . $row["comment"]. " - Gender: " . $row["gender"]. " - Date: " . $row["reg_date"] "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Website: " . $row["website"]. " - Comment: " . $row["comment"]. " - Gender: " . $row["gender"]. "<br>";
   }
 } else {
   echo "0 results";
